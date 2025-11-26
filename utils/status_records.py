a1c1d1efdfd75f3f611f40780b947341182fbe2a
Troppmop <troppmop@gmail.com>
2025-11-26 16:35:34 +0200

    http response completed, ready to merge to main

--- utils/status_records.py
+++ utils/status_records.py
@@ -8,7 +8,8 @@ def get_soldier_record(base:Base)-> list[dict]:
     for soldier in base.waiting_list:
         record = {
             'soldier_name':soldier.first_name + " " + soldier.last_name,
-            'status':soldier.placement_status 
+            'status':soldier.placement_status,
+            'note': 'soldier is on the waiting list' 
         }
         soldier_records.append(record)
 
@@ -18,7 +19,9 @@ def get_soldier_record(base:Base)-> list[dict]:
             for soldier in room.occupants:
                 record = {
                     'soldier_name':soldier.first_name + " " + soldier.last_name,
-                    'status':soldier.placement_status 
+                    'status':soldier.placement_status,
+                    'dorm': dorm.name,
+                    'room': room.number
                 }
                 soldier_records.append(record)
 
