80323b8f22cd73c612cf285c9785e5eaf3a1b243
Troppmop <troppmop@gmail.com>
2025-11-26 11:05:20 +0200

    changed route name

--- routes/upload_csv.py
+++ routes/upload_csv.py
@@ -4,6 +4,6 @@ from utils.csv_handler import process_csv
 
 router = APIRouter()
 
-@router.post("/upload-csv/")
+@router.post("/assignWithCsv/")
 def upload_csv(file: UploadFile = File()):
     return process_csv(file)
